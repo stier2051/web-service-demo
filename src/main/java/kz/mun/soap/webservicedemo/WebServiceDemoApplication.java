0e8baec91a16b006a19fd29e7ae9ca7ce90f6105
Alexandr Mun <mun_a2006@yahoo.de>
2024-01-30 13:11:46 +0600

    initial commit

---
+++ src/main/java/kz/mun/soap/webservicedemo/WebServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package kz.mun.soap.webservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/mun/soap/webservicedemo/WebServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.mun.soap.webservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
