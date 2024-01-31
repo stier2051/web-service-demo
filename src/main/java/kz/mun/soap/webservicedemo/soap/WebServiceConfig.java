0ce5133d9c68eebc7b21fda1ac120f7c6d750015
Alexandr Mun <mun_a2006@yahoo.de>
2024-01-31 09:40:27 +0600

    WS-001 added basic classes and config

---
+++ src/main/java/kz/mun/soap/webservicedemo/soap/WebServiceConfig.java
@@ -0,0 +1,21 @@
+package kz.mun.soap.webservicedemo.soap;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
+@Configuration
+@EnableWs
+public class WebServiceConfig {
+    //MessageDispatcherServlet
+    @Bean
+    ServletRegistrationBean messageDispatcherServlet (ApplicationContext context) {
+        MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+        messageDispatcherServlet.setApplicationContext(context);
+        messageDispatcherServlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(messageDispatcherServlet, "/ws/");
+    }
+}
