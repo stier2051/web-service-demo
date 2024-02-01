f8349cf36acea698fde12c9271f6b06f495a069a
Alexandr Mun <mun_a2006@yahoo.de>
2024-02-01 08:31:21 +0600

    added wsdl generator

--- src/main/java/kz/mun/soap/webservicedemo/soap/WebServiceConfig.java
+++ src/main/java/kz/mun/soap/webservicedemo/soap/WebServiceConfig.java
@@ -4,18 +4,42 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 @Configuration
 @EnableWs
-public class WebServiceConfig {
-    //MessageDispatcherServlet
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+    // MessageDispatcherServlet
+    // ApplicationContext
+    // url -> /ws/*
     @Bean
-    ServletRegistrationBean messageDispatcherServlet (ApplicationContext context) {
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
         MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
         messageDispatcherServlet.setApplicationContext(context);
         messageDispatcherServlet.setTransformWsdlLocations(true);
         return new ServletRegistrationBean(messageDispatcherServlet, "/ws/");
+    }
+
+    // /ws/courses.wsdl
+    @Bean(name = "courses")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema coursesSchema) {
+        DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+        definition.setPortTypeName("CoursePort");
+        definition.setTargetNamespace("http://mun.kz/courses");
+        definition.setLocationUri("/ws");
+        definition.setSchema(coursesSchema);
+        return definition;
+    }
+
+    @Bean
+    public XsdSchema coursesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("course-details.xsd"));
     }
 }
