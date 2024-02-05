41562df13702d56da316c1089c803b3e7a26150a
Alexandr Mun <mun_a2006@yahoo.de>
2024-02-05 10:38:55 +0600

    WS-004 added security

--- src/main/java/kz/mun/soap/webservicedemo/soap/WebServiceConfig.java
+++ src/main/java/kz/mun/soap/webservicedemo/soap/WebServiceConfig.java
@@ -7,10 +7,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
+
+import javax.security.auth.callback.CallbackHandler;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWs
@@ -41,5 +49,19 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     @Bean
     public XsdSchema coursesSchema() {
         return new SimpleXsdSchema(new ClassPathResource("course-details.xsd"));
+    }
+
+    @Bean
+    public Wss4jSecurityInterceptor securityInterceptor() {
+        Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
+        securityInterceptor.setSecurementActions("UsernameToken");
+        securityInterceptor.setSecurementUsername("user");
+        securityInterceptor.setSecurementPassword("password");
+        return securityInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(securityInterceptor());
     }
 }
